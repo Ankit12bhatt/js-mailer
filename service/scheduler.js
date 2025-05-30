b54be3c8d0efb7c943cd01b785399e9c3afa924f
dillon.ankit <dillon.ankit@rubicotech.in>
2025-05-30 21:45:56 +0530

    testing done

--- service/scheduler.js
+++ service/scheduler.js
@@ -38,7 +38,6 @@ const scheduleNotification =  () => {
         await sendEmail("📚 Your JavaScript Topics for Today", message.replace(/\n/g, '<br>'))
      }
     cron.schedule("0 6 * * *", dataFetcher, { timezone: "Asia/Kolkata" });
-    cron.schedule("* * * * *", dataFetcher, { timezone: "Asia/Kolkata" });
     cron.schedule("0 21 * * *", dataFetcher, { timezone: "Asia/Kolkata" });
 }
 module.exports = {
